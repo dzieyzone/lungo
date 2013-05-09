840cfa749a74cc312f671232cf53281dbf090d24
Jay <dzieyzone@gmail.com>
2013-05-09 16:55:25 +0800

    app

--- app/app.js
+++ app/app.js
@@ -4,18 +4,22 @@
     dataType: 'json', //'json', 'xml', 'html', or 'text'
     async: true,
     success: function(response) {
-			for(var section in response) {
-				renderThis(section, response);
-			}
-		},
+      for(var section in response) {
+        renderThis(section, response);
+      }
+    },
     error: function(xhr, type) {
-		}
+    }
   });
-	
-	//$$('aside article li').on('tap', function(event){
+
+  //$$('aside article li').on('tap', function(event){
   //  $$('aside article li').removeClass('active');
   //  $$(this).addClass('active');
   //});
+  
+  Lungo.dom('.home-aside').tap(function(event) {
+    Lungo.View.Aside.toggle("#navigation");
+  });
 
 var renderThis = function(struct, data){
   console.log(struct, data);  
