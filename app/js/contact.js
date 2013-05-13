160b236c093beb27661ecc8a4a8f0fde932dc522
joelmarkmagundayao <joel@ilovehongkong.hk>
2013-05-14 00:12:56 +0800

    update

--- app/js/contact.js
+++ app/js/contact.js
@@ -1,8 +1,16 @@
-Lungo.dom('.submit').tap(function(event) {
-  var name = $$('.name').val();
-  var mail = $$('.mail').val();
-  var message = $$('.message').val();
+Lungo.dom(".submit").tap(function(event) {
+  var name = $$(".name").val();
+  var mail = $$(".mail").val();
+  var message = $$(".message").val();
   
-  alert('Form submitted!');
+  $$.ajax({
+	    type: 'POST', // defaults to 'GET'
+	    url: "http://localhost/lungo/app/sections/contact.php?name="+name+"&mail="+mail+"&message="+message,
+	    data: {name : ""},
+	    dataType: 'html', //'json', 'xml', 'html', or 'text'
+	    async: true,
+	    success: function(response) { $$(".messages").html(response); },
+	    error: function(xhr, type) { alert(0); }
+	});
   
 });
--- app/sections/contact.php
+++ app/sections/contact.php
@@ -1,4 +1,33 @@
-<?php 
-print_r($_POST);
-print 'test';
+<?php
+  if(empty($_GET['name'])){
+    echo 'Name field is required.';	
+  }
+  elseif(empty($_GET['mail'])){
+  	echo 'Mail field is required.';
+  }
+  elseif(empty($_GET['message'])){
+  	echo 'Message field is required.';
+  }
+  elseif(!filter_var($_GET['mail'], FILTER_VALIDATE_EMAIL)){
+  	echo 'Email field should be a valid email address.';
+  }
+  else {
+  	$to = "joel.magundayao@insitesocial.com";
+  	$subject = "Frankenman Inquiry";
+  	$message = "<b>Name:</b> ".$_GET['name']."";
+  	$message .= "<b>Message:</b><br/> ".$_GET['message']."";
+  	$header = "From:".$_GET['mail']." \r\n";
+  	$header = "Cc:joel@iwoork.com \r\n";
+  	$header .= "MIME-Version: 1.0\r\n";
+  	$header .= "Content-type: text/html\r\n";
+  	$retval = mail($to,$subject,$message,$header);
+  	if( $retval == true )
+  	{
+  		echo "Message has been submitted.";
+  	}
+  	else
+  	{
+  		echo "Message could not be sent...";
+  	}
+  }
 ?>
