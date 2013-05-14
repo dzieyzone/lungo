ab4be9192549cb4448b8c3c1fe76c7ebfc55318b
Jay <dzieyzone@gmail.com>
2013-05-14 11:51:23 +0800

    player js

--- app/js/player.js
+++ app/js/player.js
@@ -14,18 +14,20 @@ Lungo.dom('#video').on('unload', function(event){
   player.stopVideo();
 });
 
-var tag = document.createElement('script');
+$$.ready(function() {
+  var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '100%',
-    width: '100%',
-    videoId: 'U88nQ16Bm1M',
-    html5: 1,
-  });
-}
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '100%',
+      width: '100%',
+      videoId: 'U88nQ16Bm1M',
+      html5: 1,
+    });
+  }
+});
